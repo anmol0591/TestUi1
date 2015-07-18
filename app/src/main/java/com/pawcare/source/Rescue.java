c3ab0219e72f574811a9241282a3446e40440bbb
Ashish Singh <a.ashish.singh@oracle.com>
2015-07-19 00:02:06 +0530

    Parse persistence

--- app/src/main/java/com/pawcare/source/Rescue.java
+++ app/src/main/java/com/pawcare/source/Rescue.java
@@ -1,271 +1,156 @@
 package com.pawcare.source;
 
-
-import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.net.Uri;
-import android.os.AsyncTask;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.provider.MediaStore;
-import android.provider.Settings;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import com.example.anm.uitest1.R;
-import com.pawcare.source.location.GPSEnableDialog;
+import com.parse.ParseException;
+import com.parse.ParseFile;
+import com.parse.ParseGeoPoint;
+import com.parse.ParseObject;
+import com.parse.ParseSession;
+import com.parse.SaveCallback;
 
+import java.util.Arrays;
 
-import com.pawcare.source.location.LocationAddress;
-import com.pawcare.source.util.ConfirmRescue;
-import com.pawcare.source.util.ImageCapture;
-
-import java.io.File;
-import java.util.Calendar;
-
-/*
-NAME
-Rescue.java
-
-DESCRIPTION
-Rescue fragment controls all the functionality on the rescue page
-
-RELATED DOCUMENTS
-None
-
-PROTOCOLS
-None
-
-EXAMPLES
-None
-
-NOTES
-None
-
-MODIFIED   (MM/DD/YY)
-Yosserian    07/17/15 - Implements Rescue Page
-*/
-public class Rescue extends android.support.v4.app.Fragment implements LocationListener {
-
-    Button btnGPSShowLocation, btnRescue; // Button for GPS Location
-    EditText tvAddress, et_more_info, et_email, et_type, et_condition, et_contact_number;
-    String str_location;
-    View view_res;
-    protected LocationManager locationManager;
-    ImageView viewImage;            // For capturing image
-    ImageButton captureImage;       // Button for updating image
-    File imageFile;
-
-    @Override
-    /**
-     * creates and returns the view hierarchy associated with the fragment.
-     * comment
+/**
+ * Created by anmumukh on 7/18/15.
+ */
+public class Rescue {
+    /*
+    Condition
+Location GeoPoint
+Type
+ImageFile
+MoreInfo
+Address
+Mail
      */
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        view_res = inflater.inflate(R.layout.rescue_layout, container, false);
-        initializeUIElements();
+    public static final String RESCUE_TYPE = "Type";
+    public static final String RESCUE_CONDITION = "Condition";
+    public static final String RESCUE_MORE_INFO = "MoreInfo";
+    public static final String RESCUE_ADDRESS = "Address";
+    public static final String RESCUE_MAIL = "Mail";
+    public static final String RESCUE_IMAGE_FILE = "ImageFile";
+    public static final String RESCUE_LOCATION = "Location";
+    public static final String RESCUE_PHONE = "Phone";
 
-        // Clicked add image button.
-        captureImage.setOnClickListener(new View.OnClickListener() {
+    private String type;
+    private String condition;
+    private String moreInfo;
+    private String address;
+    private String mail;
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    private String phone;
+    private byte[] imageFile;
+    private Location location;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public void setCondition(String condition) {
+        this.condition = condition;
+    }
+
+    public String getMoreInfo() {
+        return moreInfo;
+    }
+
+    public void setMoreInfo(String moreInfo) {
+        this.moreInfo = moreInfo;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public byte[] getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(byte[] imageFile) {
+        this.imageFile = imageFile;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public boolean persist()
+    {
+        Log.d("PAWED","persist");
+        ParseObject rescue = new ParseObject("Rescue");
+        rescue.put(RESCUE_CONDITION,condition);
+        rescue.put(RESCUE_ADDRESS,address);
+        ParseGeoPoint rescueLocation = new ParseGeoPoint(location.getLatitude(),location.getLongitude());
+        rescue.put(RESCUE_LOCATION,rescueLocation);
+        rescue.put(RESCUE_MAIL,mail);
+        rescue.put(RESCUE_MORE_INFO,moreInfo);
+        rescue.put(RESCUE_TYPE,type);
+        if (imageFile != null){
+            Log.d("PAWED", "setting image parse file");
+            rescue.put(RESCUE_IMAGE_FILE, new ParseFile("rescueImage.jpg",imageFile));
+        }
+        int x;
+        Log.d("PAWED", "CLOSE " + toString());
+        rescue.saveInBackground(new SaveCallback() {
             @Override
-            public void onClick(View v) {
-                Log.d("PAWED", "Ishita: Image button clicked.");
-                selectImageDialog();
-            }
-        });
-
-        btnGPSShowLocation.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-                Location location = null;
-
-                locationManager = (LocationManager) ((Context) getActivity()).getSystemService(Context.LOCATION_SERVICE);
-                location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                if (location != null) {
-                    LocationAddress locationAddress = new LocationAddress();
-                    str_location = locationAddress.getAddressFromLocation(location.getLatitude(), location.getLongitude(), getActivity().getApplicationContext());
-                    tvAddress.setText(str_location);
-                } else {
-                    if (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-                        GPSEnableDialog gpsEnableDialog = new GPSEnableDialog();
-                        gpsEnableDialog.show(getActivity().getSupportFragmentManager(), "gps_tag");
-                    }
-                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000 * 60, 20, Rescue.this);
-                    locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 1000 * 60, 20, Rescue.this);
-
-                }
-
-            }
-
-        });
-        btnRescue.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View arg0) {
-
-                Boolean validation = fieldValidation();
-                ConfirmRescue confirmRescue = new ConfirmRescue();
-                if (validation == true) {
-
-                    //do something
-                    confirmRescue.setMessage("Are you sure about rescuing this animal?");
-                    confirmRescue.show(getActivity().getSupportFragmentManager(), "alert");
-
-                } else {
-                    //parse rescue
-                }
-
-
-            }
-        });
-
-        return view_res;
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location) {
-        if (location != null) {
-            Log.v("Location Changed", location.getLatitude() + " and " + location.getLongitude());
-            LocationAddress locationAddress = new LocationAddress();
-            String str_location = locationAddress.getAddressFromLocation(location.getLatitude(), location.getLongitude(), getActivity().getApplicationContext());
-            tvAddress.setText(str_location);
-
-            locationManager.removeUpdates(this);
-        }
-
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        } else {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-        }
-
-    }
-
-    @Override
-    public void onProviderDisabled(String provider) {
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, this);
-        } else {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, this);
-        }
-
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK) {
-            imageFile = ImageCapture.displayImage(viewImage, data, (Context) getActivity(), requestCode);
-        }
-    }
-
-    public void initializeUIElements() {
-        btnGPSShowLocation = (Button) view_res.findViewById(R.id.btn_location);
-        btnRescue = (Button) view_res.findViewById(R.id.btn_rescue);
-        tvAddress = (EditText) view_res.findViewById(R.id.txt_location);
-        et_type = (EditText) view_res.findViewById(R.id.et_type);
-        et_email = (EditText) view_res.findViewById(R.id.et_email);
-        et_contact_number = (EditText) view_res.findViewById(R.id.et_contact_number);
-        et_condition = (EditText) view_res.findViewById(R.id.et_condition);
-        et_more_info = (EditText) view_res.findViewById(R.id.et_more_info);
-        captureImage = (ImageButton) view_res.findViewById(R.id.btn_image_capture);
-        viewImage = (ImageView) view_res.findViewById(R.id.img_capture);
-    }
-
-    public Boolean fieldValidation() {
-        Boolean validated = true;
-        if (et_type.getText().toString().length() == 0 || et_type == null) {
-            validated = false;
-            et_type.setError("Animal Type is required!");
-
-        } else if (et_condition.getText().toString().length() == 0 || et_type == null) {
-            validated = false;
-            et_condition.setError("Animal Condition is required!");
-        } else if (tvAddress.getText().toString().length() == 0 || tvAddress == null) {
-            validated = false;
-            tvAddress.setError("Location is required!");
-        } else if (et_email.getText().toString().length() == 0 || !et_email.getText().toString().contains("@")) {
-            validated = false;
-            et_email.setError("Email id is invalid!");
-
-        } else if (et_contact_number.getText().toString().length() != 10) {
-            validated = false;
-            et_contact_number.setError("Contact Number is invalid!");
-
-        }
-        return validated;
-    }
-
-    // Displays the dialog for selecting/capturing image.
-    private void selectImageDialog() {
-
-        Log.d("PAWED", "Ishita: Inside selectImage");
-
-        final CharSequence[] options = { "Take Photo", "Choose from Gallery","Cancel" };
-
-        AlertDialog.Builder builder;
-        builder = new AlertDialog.Builder((Context) getActivity());
-        builder.setTitle("Add Photo!");
-        builder.setItems(options, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                if (options[item].equals("Take Photo"))
-                {
-                    Log.d("PAWED", "Ishita: Option Take Photo");
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
-                    startActivityForResult(intent, 1);
-                }
-                else if (options[item].equals("Choose from Gallery"))
-                {
-                    Log.d("PAWED", "Ishita: Option Choose from Gallery");
-                    Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-
-                    startActivityForResult(intent, 2);
-
-                }
-                else if (options[item].equals("Cancel")) {
-                    Log.d("PAWED", "Ishita: Cancel");
-                    dialog.dismiss();
+            public void done(ParseException e) {
+                if (e != null){
+                    Log.d("PAWED","PE "+e.toString());
                 }
             }
         });
-        builder.show();
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        Log.d("PAWED","inTS");
+//        String str = "Rescue{" +
+//                "type='" + type + '\'' +
+//                ", condition='" + condition + '\'' +
+//                ", moreInfo='" + moreInfo + '\'' +
+//                ", address='" + address + '\'' +
+//                ", mail='" + mail + '\'' +
+//                ", phone='" + phone + '\'' +
+//                ", imageFile=" + Arrays.toString(imageFile) +
+//                ", location=" + location +
+//                '}';
+        Log.d("PAWED","printed");
+        return "";
     }
 }
-
-
-
-
-
-
-
-
--- app/src/main/java/com/pawcare/source/util/ConfirmRescue.java
+++ app/src/main/java/com/pawcare/source/util/ConfirmRescue.java
@@ -7,6 +7,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.pawcare.source.Rescue;
 
 /**
  * Created by mur on 7/18/2015.
@@ -16,7 +20,7 @@ public class ConfirmRescue extends android.support.v4.app.DialogFragment {
     public String setMessage(String message) {
         return this.message=message;
     }
-
+    public Rescue rescue;
     String message;
 
     public ConfirmRescue()
@@ -34,6 +38,17 @@ public class ConfirmRescue extends android.support.v4.app.DialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         //dialog.cancel();
                         //PARSE
+                        Toast toast = null;
+                        Log.d("PAWED", "before persist");
+                        Log.d("PAWED",rescue.toString());
+                        if (rescue.persist())
+                        {
+                            toast = Toast.makeText(getActivity().getApplicationContext(),"Rescue request sent.Thank you!!!",Toast.LENGTH_LONG);
+                        }
+                        else {
+                            toast = Toast.makeText(getActivity().getApplicationContext(),"Error in sending request. Please try again.",Toast.LENGTH_LONG);
+                        }
+                        toast.show();
 
                     }
                 });
