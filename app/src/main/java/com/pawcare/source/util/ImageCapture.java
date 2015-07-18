8d4ef52cb5ad9f8b3406873843e261afaf5f2cc3
ishianka <ishianka@gmai.com>
2015-07-18 22:36:06 +0530

    ImageCapture file added.

---
+++ app/src/main/java/com/pawcare/source/util/ImageCapture.java
@@ -0,0 +1,93 @@
+package com.pawcare.source.util;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.widget.ImageView;
+import java.io.File;
+import java.io.IOException;
+
+
+/**
+ * Created by igupta on 7/18/2015.
+ */
+public class ImageCapture {
+
+    /**
+     *  Displays the selected or captured image in the ImageView.
+     * @param viewImage
+     * @param data
+     * @param context
+     * @param requestCode
+     */
+    public static File displayImage(ImageView viewImage, Intent data,final Context context, int requestCode) {
+        File f = null;
+        if (requestCode == 1) {
+            Log.d("PAWED", "Ishita: Option Take Photo is selected. Save new.");
+            f = new File(Environment.getExternalStorageDirectory().toString());
+            for (File temp : f.listFiles()) {
+                if (temp.getName().equals("temp.jpg")) {
+                    f = temp;
+                    break;
+                }
+            }
+            try {
+                Bitmap bitmap;
+                BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+                bitmapOptions.inSampleSize = 2;
+
+                bitmap = BitmapFactory.decodeFile(f.getAbsolutePath(),
+                        bitmapOptions);
+
+                int height = bitmap.getHeight(), width = bitmap.getWidth();
+
+                if (height > 1280 && width > 960) {
+                    Bitmap displayBitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                    viewImage.setImageBitmap(displayBitmap);
+
+                    Log.d("PAWED", "Ishita: Bitmap too large. Resizing.");
+
+                } else {
+                    viewImage.setImageBitmap(bitmap);
+                    Log.d("PAWED", "Ishita: No need to resize.");
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } else if (requestCode == 2) {
+            Log.d("PAWED", "Ishita: Option Choose from gallery is selected.");
+            Uri selectedImage = data.getData();
+            f = new File(selectedImage.getPath());
+            Bitmap bitmap = null;
+            BitmapFactory.Options bitmapOptions = new BitmapFactory.Options();
+            bitmapOptions.inSampleSize = 2;
+
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(context.getContentResolver(), selectedImage);
+
+                int height = bitmap.getHeight(), width = bitmap.getWidth();
+
+                if (height > 1280 && width > 960) {
+                    Bitmap displayBitmap = BitmapFactory.decodeFile(f.getAbsolutePath(), bitmapOptions);
+                    viewImage.setImageBitmap(displayBitmap);
+
+                    Log.d("PAWED", "Ishita: Bitmap too large. Resizing.");
+
+                } else {
+                    viewImage.setImageBitmap(bitmap);
+                    Log.d("PAWED", "Ishita: No need to resize.");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            viewImage.setImageBitmap(bitmap);
+        }
+        return f;
+    }
+}
